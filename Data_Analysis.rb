38c049348ad71c4295eab877a312679c99d5916e
Steven Makunzva <Steven.Makunzva@imqs.co.za>
2015-12-23 15:23:36 +0200

    Added my first git file

--- Data_Analysis.rb
+++ Data_Analysis.rb
@@ -1,6 +1,5 @@
 #This excersice will provide knowledge on how to get data from a web service and calculate the mean and median from the data
 require_relative('functions')
-
 start, endd = get_UserDates();
 puts start
 puts endd
